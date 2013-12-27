ad26155bb9067b4e24b4ec9bfd881523e346bf68
Genis <Genis231@live.com>
2013-12-27 13:50:21 -0600

    modified the "drill" class so make it more efficient and hopefully cause less lag (hopefully)

--- src/ad/Genis231/Blocks/Drill.java
+++ src/ad/Genis231/Blocks/Drill.java
@@ -1,10 +1,13 @@
 package ad.Genis231.Blocks;
 
+import ad.Genis231.lib.ADLog;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockChest;
 import net.minecraft.block.BlockHopper;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.ItemStack;
@@ -19,18 +22,28 @@ public class Drill extends Block {
 	}
 	
 	public void onNeighborBlockChange(World world, int x, int y, int z, int neighbor) {
-		EntityItem entityitem;
+		
 		if (world.isBlockIndirectlyGettingPowered(x, y, z) && !world.isRemote) {
 			for (int i = y - 1; i > 0; i--) {
-				entityitem = new EntityItem(world, x, y + 1, z);
-				
-				if (world.getBlockId(x, i, z) != Block.bedrock.blockID) {
-					entityitem.dropItem(world.getBlockId(x, i, z), 1);
-					world.destroyBlock(x, i, z, false);
-					
-				}
+				blockBreak(world, x, i, z, 1);
 			}
 		}
 	}
 	
+	public void blockBreak(World world, int x, int y, int z, int area) {
+		int minX, minZ, maxX, maxZ;
+		
+		minX = x - area;
+		maxX = x + area;
+		
+		minZ = z - area;
+		maxZ = z + area;
+		
+		for (int xx = minX; xx <= maxX; xx++) {
+			for (int zz = minZ; zz <= maxZ; zz++) {
+				//Block.blocksList[l].dropBlockAsItem(world, xx, y, zz, i1, 0);
+				world.destroyBlock(xx, y, zz, true);
+			}
+		}
+	}
 }
