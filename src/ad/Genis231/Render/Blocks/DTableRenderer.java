74d9a11dbee2f5fea827021837f1c7f85c82df14
Genis231 <Genis231@live.com>
2014-04-15 11:41:59 -0500

    fixed a few stability issues

--- src/ad/Genis231/Render/Blocks/DTableRenderer.java
+++ src/ad/Genis231/Render/Blocks/DTableRenderer.java
@@ -1,11 +1,7 @@
 package ad.Genis231.Render.Blocks;
 
-import static org.lwjgl.opengl.GL11.GL_LIGHTING;
-import static org.lwjgl.opengl.GL11.glDisable;
-import static org.lwjgl.opengl.GL11.glEnable;
 import static org.lwjgl.opengl.GL11.glPopMatrix;
 import static org.lwjgl.opengl.GL11.glPushMatrix;
-import static org.lwjgl.opengl.GL11.glScalef;
 import static org.lwjgl.opengl.GL11.glTranslatef;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
@@ -22,21 +18,25 @@ import cpw.mods.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT) public class DTableRenderer extends TileEntitySpecialRenderer {
 	
 	private DTableModel model = new DTableModel();
-	public static long speed = 0x3FFF / 8;
+	public static long speed = 16383;
 	
 	@Override public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float tick) {
 		
 		if (tileEntity instanceof DTableTile) {
-			float rotation = (float) (360.0 * (System.currentTimeMillis() & speed) / speed);
+			float rotation = (float) (360.0 * (System.currentTimeMillis() & speed) / (speed/8));
 			
-//			glDisable(GL_LIGHTING);
+			//System.out.println(rotation);
+			
+			//glDisable(GL_LIGHTING);
+			
+			FMLClientHandler.instance().getClient().renderEngine.bindTexture(BlockTexture.DTable);
 			
 			renderFrame(x, y, z);
 			
-			for (int i = 1; i <= 4; i++)
+			for (int i = 1; i <= 4; i++){
 				renderPart(x, y, z, i, rotation);
-			
-//			glEnable(GL_LIGHTING);
+	}
+			// glEnable(GL_LIGHTING);
 			
 		}
 	}
@@ -45,11 +45,10 @@ import cpw.mods.fml.relauncher.SideOnly;
 		glPushMatrix();
 		
 		// Scale, Translate, Rotate
-		glScalef(1.0F, 1.0F, 1.0F);
 		glTranslatef((float) x + 0.5F, (float) y + 0.0F, (float) z + 0.5F);
 		
 		// Bind texture
-		FMLClientHandler.instance().getClient().renderEngine.bindTexture(BlockTexture.DTable);
+		// FMLClientHandler.instance().getClient().renderEngine.bindTexture(BlockTexture.DTable);
 		
 		// Render
 		model.renderFrame();
@@ -61,7 +60,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 		
 		// Scale, Translate, Rotate
 		glTranslatef((float) x + 0.5F, (float) y + 0.5F, (float) z + 0.5F);
-		
+		angle =0;
 		if (part < 3) {
 			GL11.glRotatef(part == 1 ? angle : -angle, 1F, 0F, 0F);
 		} else {
@@ -69,7 +68,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 		}
 		
 		// Bind texture
-		FMLClientHandler.instance().getClient().renderEngine.bindTexture(BlockTexture.DTable);
+		// FMLClientHandler.instance().getClient().renderEngine.bindTexture(BlockTexture.DTable);
 		
 		// Render
 		model.renderPart(part);
