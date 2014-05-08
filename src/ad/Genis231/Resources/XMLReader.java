db5af272520a07e8791afac128bed7ece337acf5
Genis231 <Genis231@live.com>
2014-05-08 03:10:15 -0500

    prepareing to redo ALL of the book node setup, the setup with be purely XML based, the reader is all setup it just needs to be implemented :)

---
+++ src/ad/Genis231/Resources/XMLReader.java
@@ -0,0 +1,76 @@
+package ad.Genis231.Resources;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import net.minecraft.util.ResourceLocation;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import ad.Genis231.Refrence.Ref;
+
+public class XMLReader {
+	HashMap<String, String> map = new HashMap<String, String>();
+	ArrayList<HashMap<String, String>> nodes = new ArrayList<HashMap<String, String>>();
+	String parent = "Object";
+	String[] keys = { "title", "tab", "posX", "posY", "gridX", "gridY", "desc", "page" };
+	
+	public void initXML(String file) {
+		try {
+			ResourceLocation stuff = new ResourceLocation(Ref.Resource_FOLDER, "SkillBook/" + file + ".artificer");
+			
+			DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+			Document doc = docBuilder.parse(Ref.getResource(stuff));
+			doc.getDocumentElement().normalize();
+			
+			NodeList listOfNodes = doc.getElementsByTagName(parent);
+			for (int i = 0; i < listOfNodes.getLength(); i++) {
+				Node currentNode = listOfNodes.item(i);
+				if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
+					map.put(keys[0], readNode(currentNode, "Name").getNodeValue().trim());
+					String[] tempDim = (readNode(currentNode, "Map").getNodeValue().trim()).split("\n");
+					
+					map.put(keys[1], tempDim[0]);
+					map.put(keys[2], tempDim[1]);
+					map.put(keys[3], tempDim[2]);
+					map.put(keys[4], tempDim[3]);
+					map.put(keys[5], tempDim[4]);
+					
+					map.put(keys[6], readNode(currentNode, "Desc").getNodeValue().trim());
+					map.put(keys[7], readNode(currentNode, "Page").getNodeValue().trim());
+					
+					nodes.add((HashMap<String, String>) map.clone());
+				}
+			}
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("File: '" + file + "' does not exist");
+		}
+		catch (Exception e) {
+			System.out.println("Had Trouble parseing file: " + file);
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<HashMap<String, String>> getNodes() {
+		return nodes;
+	}
+	
+	private Node readNode(Node currentNode, String node) {
+		Element nodeElement = (Element) currentNode;
+		
+		NodeList mappingList = nodeElement.getElementsByTagName(node);
+		Element mappingElement = (Element) mappingList.item(0);
+		
+		NodeList mappingText = mappingElement.getChildNodes();
+		return (Node) mappingText.item(0);
+	}
+}
