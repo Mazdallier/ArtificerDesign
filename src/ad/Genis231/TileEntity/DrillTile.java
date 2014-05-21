f2ef3141aab3086c68722bcc26698c303af7f547
Genis231 <Genis231@live.com>
2014-05-21 16:24:42 -0500

    fixed a lil issue with the drill's tileEntity

--- src/ad/Genis231/TileEntity/DrillTile.java
+++ src/ad/Genis231/TileEntity/DrillTile.java
@@ -16,32 +16,33 @@ import ad.Genis231.Network.Packets.DrillPacket;
 import ad.Genis231.Resources.ADTileEntity;
 import ad.Genis231.Resources.InventoryHelper;
 
-public class DrillTile extends ADTileEntity {
-	
+public class DrillTile extends ADTileEntity{
+
 	private int minX, maxX, minZ, maxZ;
 	private int cooldown = -1;
 	private int drillWidth = 5;
 	private int drillHeight = 5;
 	private int delay;
-	private Block[] blockArray = { Blocks.air, Blocks.bedrock };
+	private Block[] blockArray = {Blocks.air,Blocks.bedrock};
 	private boolean drillDone;
 	private int type = -1;
 	private int damage = 0;
 	public int angle;
 	World world;
-	
+
 	boolean needsUpdate = false;
-	
+
 	private int speed = 360 / 20;
-	
-	public DrillTile() {
+
+	public DrillTile(){
 		this.world = this.worldObj;
 		drillDone = false;
 	}
-	
-	@Override public void readFromNBT(NBTTagCompound tag) {
+
+	@Override
+	public void readFromNBT(NBTTagCompound tag){
 		super.readFromNBT(tag);
-		
+
 		drillWidth = tag.getInteger("width");
 		drillHeight = tag.getInteger("height");
 		delay = tag.getInteger("delay");
@@ -51,183 +52,185 @@ public class DrillTile extends ADTileEntity {
 		type = tag.getInteger("type");
 		damage = tag.getInteger("damage");
 	}
-	
-	@Override public void writeToNBT(NBTTagCompound tag) {
+
+	@Override
+	public void writeToNBT(NBTTagCompound tag){
 		super.writeToNBT(tag);
-		tag.setInteger("delay", delay);
-		tag.setInteger("width", drillWidth);
-		tag.setInteger("height", drillHeight);
-		tag.setBoolean("status", drillDone);
-		tag.setInteger("cd", cooldown);
-		tag.setInteger("angle", angle);
-		tag.setInteger("type", type);
-		tag.setInteger("damage", damage);
+		tag.setInteger("delay",delay);
+		tag.setInteger("width",drillWidth);
+		tag.setInteger("height",drillHeight);
+		tag.setBoolean("status",drillDone);
+		tag.setInteger("cd",cooldown);
+		tag.setInteger("angle",angle);
+		tag.setInteger("type",type);
+		tag.setInteger("damage",damage);
 	}
-	
-	public void updateEntity() {
+
+	public void updateEntity(){
 		world = this.worldObj;
-		
-		if (needsUpdate) {
+
+		if(needsUpdate){
 			needsUpdate = false;
-			world.markBlockForUpdate(xCoord, yCoord, zCoord);
+			world.markBlockForUpdate(xCoord,yCoord,zCoord);
 		}
-		
-		if (this.damage <= 0 && type != -1) {
+
+		if(this.damage <= 0 && type != -1){
 			double x = this.xCoord;
 			double y = this.yCoord;
 			double z = this.zCoord;
-			
+
 			// soundName, x, y, z, volume, pitch
-			this.worldObj.playSoundEffect(x, y, z, "random.explode", 4.0F, (1.0F + (this.worldObj.rand.nextFloat() - this.worldObj.rand.nextFloat()) * 0.2F) * 0.7F);
-			
+			this.worldObj.playSoundEffect(x,y,z,"random.explode",4.0F,(1.0F + (this.worldObj.rand.nextFloat() - this.worldObj.rand.nextFloat()) * 0.2F) * 0.7F);
+
 			// particalName minX,minY,minZ,maxX,maxY,maxZ
-			world.spawnParticle("hugeexplosion", x, y, z, 0.5, 0.5, 0.5);
+			world.spawnParticle("hugeexplosion",x,y,z,0.5,0.5,0.5);
 			this.type = -1;
 		}
-		
-		if (canDrill()) {
+
+		if(canDrill()){
 			angle = angle >= 360 ? 0 : angle + speed * 2;
-			
-			if (cooldown == 0 && !this.worldObj.isRemote) {
+
+			if(cooldown == 0 && !this.worldObj.isRemote){
 				setSize();
 				drill();
 				this.cooldown = delay;
-			} else
-				cooldown--;
-		} else
-			this.cooldown = delay;
-		
+			}else cooldown--;
+		}else this.cooldown = delay;
+
 	}
-	
-	private boolean canDrill() {
-		return world.isBlockIndirectlyGettingPowered(xCoord, yCoord, zCoord) && !drillDone && this.type != -1;
+
+	private boolean canDrill(){
+		return world.isBlockIndirectlyGettingPowered(xCoord,yCoord,zCoord) && !drillDone && this.type != -1;
 	}
-	
-	private void setSize() {
+
+	private void setSize(){
 		minX = xCoord - drillWidth;
 		maxX = xCoord + drillWidth;
-		
+
 		minZ = zCoord - drillHeight;
 		maxZ = zCoord + drillHeight;
-		
+
 		int temp;
-		if (minX > maxX) {
+		if(minX > maxX){
 			temp = minX;
 			minX = maxX;
 			maxX = temp;
 		}
-		
-		if (minZ > maxZ) {
+
+		if(minZ > maxZ){
 			temp = minZ;
 			minZ = maxZ;
 			maxZ = temp;
 		}
 	}
-	
-	private void drill() {
+
+	private void drill(){
 		int count = 0;
-		
-		if (!drillDone && !this.worldObj.isRemote)
-			for (int y = yCoord - 1; y >= 0; y--)
-				for (int x = minX; x <= maxX-1; x++)
-					for (int z = minZ; z <= maxZ-1; z++) {
-						if (isBreakable(x, y, z)) {
-							this.damage--;
-							Artificer.packets.sendToAll(new DrillPacket(type, damage, this.xCoord, this.yCoord, this.zCoord));
-							
-							addBlock(x, y, z);
-							world.setBlockToAir(x, y, z);
-							count++;
-						}
-						
-						if (count > 0)
-							return;
+
+		if(!drillDone && !this.worldObj.isRemote) for(int y = yCoord - 1; y >= 0; y--)
+			for(int x = minX; x <= maxX - 1; x++)
+				for(int z = minZ; z <= maxZ - 1; z++){
+					if(isBreakable(x,y,z)){
+						this.damage--;
+						Artificer.packets.sendToAll(new DrillPacket(type,damage,this.xCoord,this.yCoord,this.zCoord));
+
+						addBlock(x,y,z);
+						world.setBlockToAir(x,y,z);
+						count++;
 					}
-		
+
+					if(count > 0) return;
+				}
+
 		drillDone = count == 0;
-		
+
 		return;
 	}
-	
-	private void addBlock(int x, int y, int z) {
-		Block block = world.getBlock(x, y, z);
-		int meta = world.getBlockMetadata(x, y, z);
-		
-		Item tempItem = block.getItemDropped(meta, world.rand, 0);
+
+	private void addBlock(int x,int y,int z){
+		Block block = world.getBlock(x,y,z);
+		int meta = world.getBlockMetadata(x,y,z);
+
+		Item tempItem = block.getItemDropped(meta,world.rand,0);
 		int stacksize = block.quantityDropped(world.rand);
 		int damage = block.damageDropped(meta);
-		
-		if (tempItem != null && stacksize != 0) {
-			ItemStack item = new ItemStack(tempItem, stacksize, damage);
-			IInventory chest = InventoryHelper.getInventoryAround(world, this.xCoord, this.yCoord, this.zCoord, item);
-			InventoryHelper.addBlock(chest, item);
+
+		if(tempItem != null && stacksize != 0 && block.getBlockHardness(world,x,y,z) >= 0){
+			ItemStack item = new ItemStack(tempItem,stacksize,damage);
+			IInventory chest = InventoryHelper.getInventoryAround(world,this.xCoord,this.yCoord,this.zCoord,item);
+			InventoryHelper.addBlock(chest,item);
 		}
 	}
-	
-	private boolean isBreakable(int x, int y, int z) {
-		for (Block block : blockArray) {
-			if (this.worldObj.getBlock(x, y, z) == block) { return false; }
+
+	private boolean isBreakable(int x,int y,int z){
+		for(Block block : blockArray){
+			if(this.worldObj.getBlock(x,y,z) == block){
+				return false;
+			}
 		}
-		
-		return this.worldObj.getTileEntity(x, y, z) == null && this.worldObj.getBlock(x, y, z).getMaterial() != Material.water && this.worldObj.getBlock(x, y, z).getMaterial() != Material.lava;
+
+		return this.worldObj.getTileEntity(x,y,z) == null && this.worldObj.getBlock(x,y,z).getMaterial() != Material.water && this.worldObj.getBlock(x,y,z).getMaterial() != Material.lava;
 	}
-	
-	public void setStats(int width, int height, int rate) {
+
+	public void setStats(int width,int height,int rate){
 		this.drillWidth = width;
 		this.drillHeight = height;
 		this.delay = rate;
-		
+
 		needsUpdate = true;
 	}
-	
-	public void setDrill(int type, int damage) {
+
+	public void setDrill(int type,int damage){
 		this.type = type;
 		this.damage = damage;
-		
+
 		needsUpdate = true;
 	}
-	
-	public int getWidth() {
+
+	public int getWidth(){
 		return this.drillWidth;
 	}
-	
-	public int getHeight() {
+
+	public int getHeight(){
 		return this.drillHeight;
 	}
-	
-	public int getDelay() {
+
+	public int getDelay(){
 		return this.delay;
 	}
-	
-	public int getDrillType() {
+
+	public int getDrillType(){
 		return this.type;
 	}
-	
-	public int getDrillDamage() {
+
+	public int getDrillDamage(){
 		return this.damage;
 	}
-	
-	public int getAngle() {
+
+	public int getAngle(){
 		return this.angle;
 	}
-	
-	@Override public void markDirty() {
+
+	@Override
+	public void markDirty(){
 		updateEntity();
 		super.markDirty();
 		needsUpdate = true;
 	}
-	
-	@Override public Packet getDescriptionPacket() {
+
+	@Override
+	public Packet getDescriptionPacket(){
 		NBTTagCompound tag = new NBTTagCompound();
 		writeToNBT(tag);
-		return new S35PacketUpdateTileEntity(xCoord, yCoord, zCoord, 1, tag);
+		return new S35PacketUpdateTileEntity(xCoord,yCoord,zCoord,1,tag);
 	}
-	
-	@Override public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity packet) {
+
+	@Override
+	public void onDataPacket(NetworkManager net,S35PacketUpdateTileEntity packet){
 		readFromNBT(packet.func_148857_g());
 		markDirty();
-		world.markBlockRangeForRenderUpdate(xCoord, yCoord, zCoord, xCoord, yCoord, zCoord);
+		world.markBlockRangeForRenderUpdate(xCoord,yCoord,zCoord,xCoord,yCoord,zCoord);
 		this.needsUpdate = true;
 	}
-	
+
 }
